457192d56c21159a1ef1efeacea3aab4c52a0087
Victor W <victor.westerberg@yh.nackademin.se>
2026-05-08 10:50:18 +0200

    add: alert

--- src/alert.py
+++ src/alert.py
@@ -6,21 +6,29 @@ load_dotenv()
 
 WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 
-#if anomaly_score > 0.8:
-#    send_alert("🚨 CRITICAL anomaly detected!")
-#elif anomaly_score > 0.6:
-#    send_alert("⚠️ Warning anomaly detected")
 
-def send_alert(message):
+def send_anomaly_alert(message):
     data = {
-        "content": message
+        "content":  "❗️ " + message
     }
-
     requests.post(WEBHOOK_URL, json=data)
 
 
+def send_overload_alert():
+    data = {
+        "content": "📈 Overload detected!"
+    }
+    requests.post(WEBHOOK_URL, json=data)
+
+
+def send_exception_alert(message):
+    data = {
+        "content":  "⛔️ System error: " + message
+    }
+    requests.post(WEBHOOK_URL, json=data)
+
 # TEST
-send_alert("⚠️ Anomaly detected!")
+#send_overload_alert()
 
 
 
